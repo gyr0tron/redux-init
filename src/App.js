b0d1723c6166bbd60f8cccfec31d5197f1c08fc9
Gyrotron <simmarkalsi@gmail.com>
2019-02-01 03:17:36 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { updateUser, apiRequest } from './actions/user-actions';
+
+import { createSelector } from 'reselect';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+  componentDidMount() {
+    // this.props.onApiRequest();
+  }
+
+  onUpdateUser(event) {
+    this.props.onUpdateUser(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,18 +29,51 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input onChange={this.onUpdateUser} />
+          {this.props.user}
         </header>
       </div>
     );
   }
 }
 
-export default App;
+const productsSelector = createSelector(
+  state => state.products,
+  products => products
+);
+
+const userSelector = createSelector(
+  state => state.user,
+  user => user
+);
+
+const mapStateToProps = createSelector(
+  productsSelector,
+  userSelector,
+  (products, user) => ({
+    products,
+    user
+  })
+);
+
+// const mapStateToProps = (state, props) => {
+//   return {
+//     products: state.products,
+//     user: state.user,
+//     userPlusProp: `${state.user} ${props.aRandomProps}`
+//   }
+// };
+
+const mapActionsToProps = {
+    onUpdateUser: updateUser,
+    onApiRequest: apiRequest
+};
+
+// const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
+//   console.log('propsFromState: ', propsFromState);
+//   console.log('propsFromDispatch: ', propsFromDispatch);
+//   console.log('ownProps: ', ownProps);
+//   return {};
+// }
+
+export default connect(mapStateToProps, mapActionsToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,38 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import thunk from 'redux-thunk';
+import { applyMiddleware, compose, combineReducers, createStore} from 'redux';
+import { Provider } from 'react-redux';
+
+import productsReducer from './reducers/products-reducer';
+import userReducer from './reducers/user-reducer';
+
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+
+const allStoreEnhancers = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const store = createStore(
+  allReducers,
+  {
+    products: [{ name: 'iPhone' }],
+    user: 'Sam'
+  },
+  allStoreEnhancers
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App aRandomProps="whatever" />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
